5908c78cb665cf67128d80618df407b845640410
H8ER <nik.veras@mail.ru>
2020-06-01 10:40:15 +0900

    1st stage

---
+++ store/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def store(request):
+    context = {}
+    return render(request, 'store/Store.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'store/Cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/Checkout.html', context)
